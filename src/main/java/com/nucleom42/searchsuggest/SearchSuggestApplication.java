094651d0b042c7b4dfa4cdf49857f02cc14e8958
oleg <oleg@footballticketnet.com>
2018-12-14 12:39:30 +0200

    start

---
+++ src/main/java/com/nucleom42/searchsuggest/SearchSuggestApplication.java
@@ -0,0 +1,14 @@
+package com.nucleom42.searchsuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchSuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchSuggestApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/nucleom42/searchsuggest/SearchSuggestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nucleom42.searchsuggest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchSuggestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
